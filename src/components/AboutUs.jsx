553915e64c9c0362c6030eef97a0ac86e78cb6e3
Rafa Mateo <rafa.mateo@mailtrack.io>
2025-01-27 16:17:48 +0100

    about us section

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,3 +1,27 @@
 export const AboutUs = () => {
-    return <section>About us</section>;
+    return (
+        <section className="about-us">
+            <article>
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+                <p>
+                    At the heart of our restaurant is a shared passion for food
+                    and community. Founded by Adrian and Mario, two lifelong
+                    friends with a love for culinary creativity, our mission is
+                    to bring people together through delicious, diverse dishes
+                    crafted with care. Adrian brings his expertise in fresh,
+                    wholesome ingredients, while Mario infuses the menu with
+                    bold flavors and classic recipes reimagined. Together,
+                    they&apos;ve created a space where everyone feels at
+                    home—whether you&apos;re stopping by for a quick bite or
+                    celebrating a special occasion. We believe that great food
+                    has the power to create unforgettable memories, and
+                    we&apos;re honored to share our vision with the vibrant city
+                    of Chicago. Welcome to our table—where every dish is made
+                    with love and every guest is family!
+                </p>
+            </article>
+            <img src="/images/Mario and Adrian A.jpg" width={'100%'} />
+        </section>
+    );
 };
